425e85bff25a5c7ea842fc4ccab2b38a0a026444
bomwerk <81857811+Ralaimihoatra@users.noreply.github.com>
2021-11-22 17:57:40 +0100

    Modification à ce jour

--- index.php
+++ index.php
@@ -1,79 +1,84 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-    <title>Mon CV de "Développeur Web et Web Mobile"</title>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./Assets/Css/index.css"/>
-</head>
- <body>
-  <header>
-    <section> 
-     <div  class="box-container">
-      <div class="contact0"> 
-          <div class="contact">
-              <p class="p1">Nom:  &#160;</p>
-              <p class="p2">Joseph Ralaimihoatra</p>
-         </div>
-        <!--<p1 class="dodger-blue-text">I am dodger blue!</p1>-->
-          <div class="contact">
-              <p class="p1">Mail:   &#160;</p>
-              <p class="p2"><a href="joseph.ralai@netcourrier.com">joseph.ralai@netcourrier.com</a></p>
-          </div>
-          <div class="contact">
-              <p class="p1">Tél:  &#160;</p>
-              <p class="p2">06.76.41.33.52</p>
-          </div>
-          <div class="contact">
-              <p class="p1">Github:  &#160;</p>
-              <p class="p2">git@github.com:Ralaimihoatra/PROJET_CV.git</p>
-          </div>
-          <div class="Picture">
-              <img class="maphoto" src="./Assets/Picture/My_photo.JPG" alt="my_photo">
-          </div>
-     <img src="./Assets/Pictures/My photo.JPG"/>
-       </div>
-     </div>
-    </section>
-
-      <div>
-          <div class="metier">
-              <h1 class="poste"><em>Développeur Web & Web Mobile junior</em></h1>
-          </div>
-      </div>
-  </header>
- <main> 
-    <section>
-              <h2>Compétences professionnelles</h2>
-                 <div class="competence">
-                        
+    <head>
+        <title>Mon CV de "Développeur Web et Web Mobile"</title>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="./Assets/Css/index.css"/>
+    </head>
+    <body>
+        <header>        
+            <section> 
+                <div  class="box-container">
+                    <div class="contact0"> 
+                        <div class="contact">
+                            <p class="p1">Nom:  &#160;</p>
+                            <p class="p2">Joseph Ralaimihoatra</p>
+                        </div>
+                        <!--<p1 class="dodger-blue-text">I am dodger blue!</p1>-->
+                        <div class="contact">
+                            <p class="p1">Mail:   &#160;</p>
+                            <p class="p2"><a href="joseph.ralai@netcourrier.com">joseph.ralai@netcourrier.com</a></p>
+                        </div>
+                        <div class="contact">
+                            <p class="p1">Tél:  &#160;</p>
+                            <p class="p2">06.76.41.33.52</p>
+                        </div>
+                        <div class="contact">
+                            <p class="p1">Github:  &#160;</p>
+                            <p class="p2"><a href="git@github.com:Ralaimihoatra/PROJET_CV.git">git@github.com:Ralaimihoatra/PROJET_CV.git</a></p>
+                            <!--<p class="p2">git@github.com:Ralaimihoatra/PROJET_CV.git</p>-->
+                        </div>
+                        <div class="Picture">
+                            <img class="maphoto" src="./Assets/Picture/My_photo.JPG" alt="my_photo">
+                        </div><br />
+                    </div>
+                </div>
+            </section>
+                <div>
+                    <div class="metier">
+                    <h1 class="poste"><em>Développeur Web & Web Mobile junior</em></h1>
+                    </div>
+                </div>
+        </header>
+        <main> 
+            <section>
+                    <h2>Compétences professionnelles</h2>
+                        <div class="competence">        
                             <div class="logo"><a href="html&css"><img class="imglogo" src="./Assets/Picture/htmlcss-logo.svg" alt="logo1"></a></div>
                             <div class="logo"><a href="wordpress"><img class="imglogo" src="./Assets/Picture/wordpress_logo.png" alt="logo0"></a></div>
-                            <div class="logo"><a href="php"><img class="imglogo" src="./Assets/Picture/php_logo.png" alt="logo"></a></div>
-                        
-                 </div>
-                 <div>
+                            <div class="logo"><a href="php"><img class="imglogo" src="./Assets/Picture/php_logo.png" alt="logo"></a></div>        
+                        </div>
                         <div class="competence">
                             <ul>
                                 <li>Création du site Web Wordpress</li>
                                 <li>Manupilation des langages HTML5, CSS3 et PHP pour créer du site web</li>                  
-                                <li>Utilisation de la méthode SCRUM (suivant TRELLO) pour la gestion de projets</li>
+                                <li>Utilisation de la méthode SCRUM (outil de gestion:TRELLO) pour la gestion de projets</li>
                                 <li>Bon sens relationnel, organisé, autonome et travail en équipe</li>
                                 <li>En relation avec les clients & fournisseurs</li>
                                 <li>Anglais TOEIC niveau A2</li>
                                 <li>Aisance avec les outils: Visual Studio -logiciel SAGE -Excel -Word -PowerPoint</li>
                             </ul>
                         </div>
-                 </div>
-  
+            </section>
+            <section>
+                    <h2>Expériences professionnelles</h2>
+                        <div class="experience">
+                                <div class="logo">
+                                    <a href="ITESA"><img class="imglogo" src="./Assets/Picture/alta1.png" alt="logo2"></a>
+                                </div>
+                                <div class="experience2">
+                                    <h3>stage</h3>
+                                    <h4>Novembre-Décembre 2021</h4>
+                                    <h5>Assainissement</h5>
+                                </div>
+                                <div class="activites">
+                                    <p>Conception du site ALTA STONE avec WooCommerce</p>
+                                </div> 
+                        </div> 
 
-    </section>
-
-
-    <section>
-              <h2>Expériences professionnelles</h2>
-                    <div class="experience">
+                        <div class="experience">
                             <div class="logo">
                                 <a href="gurdjian"><img class="imglogo" src="./Assets/Picture/Gurdjian.png" alt="logo1"></a>
                             </div>
@@ -83,26 +88,11 @@
                                 <h5 class="experienceh5">Magasin électromenager</h5>
                             </div>
                             <div class="activites">
-                                    <p>J'ai assuré la mise à jour du site Internet de la boutique en ligne.</p>
+                                <p>J'ai assuré la mise à jour du site Internet de la boutique en ligne.</p>
                             </div>
-                    </div>
+                        </div>
 
-                    <div class="experience">
-                            <div class="logo">
-                                <a href="ITESA"><img class="imglogo" src="./Assets/Picture/Itesa.jpg" alt="logo2"></a>
-                            </div>
-                            <div class="experience2">
-                                <h3>stage</h3>
-                                <h4>Janvier 2020</h4>
-                                <h5>Commerce des alarmes</h5>
-                            </div>
-                            <div class="activites">
-                                    <p>J'ai préparé un rapport d'étonnement afin de définir et traiter un sujet d'étude de cas.<br>
-                                    Cette pratique est très utile pour pouvoir apporter plutard des solutions et d'améliorations adéquates.</p>
-                            </div> 
-                    </div>          
-
-                    <div class="experience">
+                        <div class="experience">
                             <div class="logo">
                                 <a href="sas_Ata"><img class="imglogo" src="./Assets/Picture/Ata.png" alt="logo3"></a>
                             </div>
@@ -112,14 +102,14 @@
                                 <h5>Fabrication des Taximètres</h5>
                             </div>
                             <div class="activites">
-                                    <p>J'ai mis en œuvre la méthode « Mini-Maxi » pour planifier et positionner les commandes. J'ai également fait le suivi auprès des fournisseurs.
-                                Ainsi, j'ai redimensionné notre entrepôt pour optimiser les contraintes de stockage dans le but de réduire les pertes de temps et la fatigue des opérateurs en appliquant 
-                                des différentes methodes comme SMB (Standards de Manutention de Base).<br>
-                                Ces différentes activités sont le cœur de mon domaine de compétences.</p>
+                                <p>J'ai mis en œuvre la méthode « Mini-Maxi » pour planifier et positionner les commandes. J'ai également fait le suivi auprès des fournisseurs.
+                                    Ainsi, j'ai redimensionné notre entrepôt pour optimiser les contraintes de stockage dans le but de réduire les pertes de temps et la fatigue des opérateurs en appliquant 
+                                    des différentes methodes comme SMB (Standards de Manutention de Base).<br>
+                                    Ces différentes activités sont le cœur de mon domaine de compétences.</p>
                             </div>
-                    </div>
-      
-                    <div class="experience">
+                        </div>
+            
+                        <div class="experience">
                             <div class="logo">
                                 <a href="sas_Mika"><img class="imglogo" src="./Assets/Picture/Mika1.png" alt="logo4"></a>
                             </div>
@@ -128,13 +118,13 @@
                                 <h4>2006 à 2009</h4>
                                 <h5>Magasin de bazar</h5>
                             </div>
-                        <div class="activites">
+                            <div class="activites">
                                 <p>J'ai participé à la mise à disposition des articles dans le magasin, 
-                                puis effectué la tenue de caisse. J'ai aussi renseigné et conseillé les clients.</p> 
-                        </div> 
-                    </div>
+                                    puis effectué la tenue de caisse. J'ai aussi renseigné et conseillé les clients.</p> 
+                            </div> 
+                            </div>
 
-                    <div class="experience">
+                        <div class="experience">
                             <div class="logo">
                                 <a href="sas_HOLTEX"><img class="imglogo" src="./Assets/Picture/Holtex.jpg" alt="logo5"></a>
                             </div>
@@ -143,16 +133,15 @@
                                 <h4>2003 à 2006</h4>
                                 <h5>Commerce des instrumrnts médicaux</h5>
                             </div>
-                        <div class="activites">
+                            <div class="activites">
                                 <p>J'ai assuré la tenue de stocks informatiquement et physiquement en utilisant le Logiciel
-                                de gestion et en conduisant le chariot élévateur.</p>
-                        </div> 
-                    </div>
-    </section>
-
-    <section>
-              <h2>Formations & Diplômes</h2>
-              <div class="formationdiplome">
+                                    de gestion et en conduisant le chariot élévateur.</p>
+                            </div> 
+                        </div>
+            </section>
+            <section>
+                    <h2>Formations & Diplômes</h2>
+                        <div class="formationdiplome">
                             <div class="logo">
                                 <a href="afpa"><img class="imglogo" src="./Assets/Picture/Afpa.png" alt="logo1"></a>
                             </div>
@@ -161,9 +150,9 @@
                                 <h4 class="experienceh4">2020 à 2021</h4>
                                 <h5 class="experienceh5">Afpa Saint Jerôme à Marseille</h5>
                             </div>
-              </div>
+                        </div>
 
-              <div class="formationdiplome">
+                        <div class="formationdiplome">
                             <div class="logo">
                                 <a href="Isteli"><img class="imglogo" src="./Assets/Picture/Isteli.jpg" alt="logo1"></a>
                             </div>
@@ -172,9 +161,9 @@
                                 <h4 class="experienceh4">2019 à 2020</h4>
                                 <h5 class="experienceh5">ISTELI à Marseille</h5>
                             </div>
-              </div>
+                        </div>
 
-              <div class="formationdiplome">
+                        <div class="formationdiplome">
                             <div class="logo">
                                 <a href="Adif"><img class="imglogo" src="./Assets/Picture/Adif1.jpg" alt="logo1"></a>
                             </div>
@@ -183,9 +172,9 @@
                                 <h4>2002 à 2003</h4> 
                                 <h5>ADIF Aubagne</h5>
                             </div>
-              </div>
+                        </div>
 
-              <div class="formationdiplome">
+                        <div class="formationdiplome">
                             <div class="logo">
                                 <a href="Merkure"><img class="imglogo" src="./Assets/Picture/Merkure.jpg" alt="logo1"></a>
                             </div>
@@ -194,9 +183,9 @@
                                 <h4>1995 à 1996</h4>
                                 <h5>Groupe Merkure à Aix-en-Provence</h5>  
                             </div>
-              </div>
+                        </div>
 
-              <div class="formationdiplome">  
+                        <div class="formationdiplome">  
                             <div class="logo">
                                 <a href="Lycee"><img class="imglogo" src="./Assets/Picture/Lycée.png" alt="logo1"></a>
                             </div>
@@ -204,44 +193,42 @@
                                 <h3>Titulaire Baccalauréat série S</h3>
                                 <h4>1992</h4>
                             </div>
-              </div> 
+                        </div> 
+            </section>       
+        </main>
 
-      </section>       
-</main>
-
-  <footer>
-    <section>
-            <h2>Divers</h2>
-              <div class="divers">
-                        <div class="logo">
+        <footer>
+            <section>
+                    <h2>Divers</h2>
+                        <div class="divers">
+                            <div class="logo">
                             <a href="permisB"><img class="imglogo" src="./Assets/Picture/PermisB.png" alt="logo1"></a>
+                            </div>
+                            <div class="experience2">
+                                <h3>Titulaire permis B</h3>
+                                <h4>Véhiculé</h4>
+                            </div>
                         </div>
-                        <div class="experience2">
-                            <h3>Titulaire permis B</h3>
-                            <h4>Véhiculé</h4>
-                        </div>
-              </div>
 
-              <div class="divers">
-                        <div class="logo">
-                            <a href="petanque"><img class="imglogo" src="./Assets/Picture/Pétanque.jpg" alt="logo1"></a>
+                        <div class="divers">
+                            <div class="logo">
+                                <a href="petanque"><img class="imglogo" src="./Assets/Picture/Pétanque.jpg" alt="logo1"></a>
+                            </div>
+                            <div class="experience2">
+                                <h3>Passionné du jeu de pétanque</h3>
+                                <h4>participé aux petits concours</h4>
+                            </div>
                         </div>
-                        <div class="experience2">
-                            <h3>Passionné du jeu de pétanque</h3>
-                            <h4>participé aux petits concours</h4>
-                        </div>
-              </div>
 
-              <div class="divers">
-                        <div class="logo">
-                            <a href="laWii"><img class="imglogo" src="./Assets/Picture/Lawii.jpg" alt="logo1"></a>
-                        </div>
-                        <div class="experience2">
-                            <h3>Passionné du jeu de La wii</h3>
-                        </div>
-              </div>                     
-                
-    </section>           
-  </footer>
- </body>
+                        <div class="divers">
+                            <div class="logo">
+                                <a href="laWii"><img class="imglogo" src="./Assets/Picture/Lawii.jpg" alt="logo1"></a>
+                            </div>
+                            <div class="experience2">
+                                <h3>Passionné du jeu de La wii</h3>
+                            </div>
+                        </div>                                             
+            </section>           
+        </footer>
+    </body>
 </html>
